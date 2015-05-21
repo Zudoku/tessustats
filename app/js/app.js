346397757261e24e5fae5a9b6388976c7dce8a41
PullaPoski <arttu.siren@gmail.com>
2015-05-21 17:20:44 +0300

    Started developing the country and channel pages

--- app/js/app.js
+++ app/js/app.js
@@ -8,11 +8,19 @@ angular.module('myApp', [
   'myApp.services',
   'myApp.directives',
   'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
+]).config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/index', {templateUrl: 'partials/front.html', controller: 'MyCtrl1'});
+  //SERVER
   $routeProvider.when('/server', {templateUrl: 'partials/server.html', controller: 'MyCtrl1'});
+  //USER
   $routeProvider.when('/users', {templateUrl: 'partials/users.html', controller: 'usersCtrl'});
   $routeProvider.when('/user/:databaseid', {templateUrl: 'partials/user.html', controller: 'userCtrl'});
+  //CHANNEL
+  $routeProvider.when('/channels', {templateUrl: 'partials/channels.html', controller: 'channelsCtrl'});
+  $routeProvider.when('/channel/:channelname', {templateUrl: 'partials/channel.html', controller: 'channelCtrl'});
+  //COUNTRY
+  $routeProvider.when('/countries', {templateUrl: 'partials/countries.html', controller: 'countriesCtrl'});
+  $routeProvider.when('/country/:countrycode', {templateUrl: 'partials/country.html', controller: 'countryCtrl'});
+  //FRONT PAGE
   $routeProvider.otherwise({templateUrl: 'partials/front.html', controller: 'MyCtrl1'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -16,6 +16,47 @@ angular.module('myApp.controllers', [])
 
 } ])
 
+
+.controller('countriesCtrl', ['$scope','$http','$location', function($scope, $http,$location) {
+		var countryResource = $http.get('/allcountries').success(function(data) {
+			$scope.countries = data
+		})
+
+		$scope.selectcountry=function(countrycode){
+
+			$location.path('/country/'+countrycode);
+		}
+
+} ])
+
+.controller('countryCtrl', ['$scope','$http','$location','$routeParams', function($scope, $http,$location,$routeParams) {
+		var countryResource = $http.get('/country/'+$routeParams.countrycode).success(function(data) {
+			$scope.country = data;
+
+		})
+
+} ])
+
+.controller('channelsCtrl', ['$scope','$http','$location', function($scope, $http,$location) {
+		var countryResource = $http.get('/allchannels').success(function(data) {
+			$scope.channels = data
+		})
+
+		$scope.selectchannel=function(channelname){
+
+			$location.path('/channel/'+channelname);
+		}
+
+} ])
+
+.controller('channelCtrl', ['$scope','$http','$location','$routeParams', function($scope, $http,$location,$routeParams) {
+		var channelResource = $http.get('/channel/'+$routeParams.channelname).success(function(data) {
+			$scope.channel = data;
+
+		})
+
+} ])
+
 .controller('userCtrl', ['$scope','$http','$location','$routeParams', function($scope, $http,$location,$routeParams) {
 	var userResource = $http.get('/user/'+$routeParams.databaseid).success(function(data) {
 		$scope.user = data;
