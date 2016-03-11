ea05aa3126770fee2d1cb6def6fe1cc669331452
Arttu Siren <arttu.siren@gmail.com>
2016-03-11 22:17:59 +0200

    shit shit hsit

--- server/config.js
+++ server/config.js
@@ -1,17 +1,19 @@
 var config = {};
 
 config.webserver_port = 3700;
-config.webserver_bind = '127.0.0.1';
+config.webserver_bind = '0.0.0.0';
 
 config.timeout_between_scans = 300000; //ms
-config.serverquery_username = "ExampleUsername";
-config.serverquery_password = "ExamplePassword"; 
+config.serverquery_username = "Rivenation";
+config.serverquery_password = "txYWcuUd"; 
 config.virtual_server_id = 1;
-config.ts_ip = "ExampleTSIP";
+config.ts_ip = "loungestats.com";
 config.time_between_queries = 1000; //milliseconds
 
 config.database_path = 'server/db';
 
 config.mode = "";
+config.debug_network = false;
 
 module.exports = config;
+
